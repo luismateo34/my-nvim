8cf38f76b0ce0032ab8a81d998c2b2d0a833f876
Luis Piñeiro <luismateo_34@hotmail.com>
2023-05-23 05:34:52 -0300

    null-ls listo

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,17 +1,21 @@
 return {
 	"jose-elias-alvarez/null-ls.nvim",
+	dependencies = { "mason.nvim", "nvim-lua/plenary.nvim" },
 	event = "BufReadPre",
-	dependencies = { "mason.nvim", "MunifTanjim/prettier.nvim" },
 	opts = function()
 		local nls = require("null-ls")
-		return {
+		return{
 			sources = {
 				nls.builtins.formatting.stylua.with({
 					filetypes = { "lua" },
 				}),
 				nls.builtins.diagnostics.flake8,
+				--nls.builtins.diagnostics.eslint,
+				--null_ls.builtins.diagnostics.eslint_d.with({
+				--    diagnostics_format = '[eslint] #{m}\n(#{c})'
+				--}),
 				nls.builtins.diagnostics.eslint.with({
-					-- ignore prettier warnings from eslint-plugin-prettier
+					---- ignore prettier warnings from eslint-plugin-prettier
 					filter = function(diagnostic)
 						return diagnostic.code ~= "prettier/prettier"
 					end,
