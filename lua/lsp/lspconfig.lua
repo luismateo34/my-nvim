cec232f801172a4ff91cb2f4d3b44f47f10b7d25
Luis Piñeiro <luismateo_34@hotmail.com>
2023-07-13 21:14:03 -0300

    limpiando archivo lspconfig

--- lua/lsp/lspconfig.lua
+++ lua/lsp/lspconfig.lua
@@ -7,17 +7,6 @@ end
 
 local protocol = require("vim.lsp.protocol")
 
---local augroup_format = vim.api.nvim_create_augroup("Format", { clear = true })
---local enable_format_on_save = function(_, bufnr)
-	--vim.api.nvim_clear_autocmds({ group = augroup_format, buffer = bufnr })
-	--vim.api.nvim_create_autocmd("BufWritePre", {
-		--group = augroup_format,
-		--buffer = bufnr,
-		--callback = function()
-			--vim.lsp.buf.format({ bufnr = bufnr })
-		--end,
-	--})
---end
 
 local on_attach = function()
 	vim.api.nvim_create_autocmd("LspAttach", {
