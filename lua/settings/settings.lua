84278b439a3b0ddc2784d59c9d0bde0ac04da442
Luis Piñeiro <luismateo_34@hotmail.com>
2023-08-23 20:46:17 -0300

    ale optimizado para linter

--- lua/settings/settings.lua
+++ lua/settings/settings.lua
@@ -45,9 +45,15 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 		vim.cmd([[%s/\s\+$//e]])
 	end,
 })
+
+--local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 vim.cmd([[
+let g:ale_use_neovim_diagnostics_api = 1
+let g:ale_disable_lsp = 1
 let b:ale_fixers = ['eslint']
 let g:ale_fix_on_save = 1
 let g:ale_completion_autoimport = 0
 let g:ale_completion_enabled = 0
+let g:ale_sign_error = " "
+let g:ale_sign_warning = " "
 ]])
