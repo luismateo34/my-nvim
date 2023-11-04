613a9b98ddf03704c1afc5c8e939d69d4cc1949b
luis mateo piñeiro <luismateo_34@hotmail.com>
2023-11-04 05:25:07 -0300

    ale configurado para typescript

--- lua/settings/aleConfig.lua
+++ lua/settings/aleConfig.lua
@@ -1,7 +1,8 @@
 vim.cmd([[
 let g:ale_use_neovim_diagnostics_api = 1
 let g:ale_disable_lsp = 1
-let b:ale_fixers = ['eslint']
+let b:ale_fixers = {'javascript': ['eslint']}
+let b:ale_fixers = {'typescript': ['eslint']}
 let g:ale_fix_on_save = 1
 let g:ale_completion_autoimport = 0
 let g:ale_completion_enabled = 0
