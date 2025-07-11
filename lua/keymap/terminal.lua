23c167e673526104598451b9db032102130b5015
luis <luismateo3461@gmail.com>
2025-07-11 07:53:12 -0300

    termianl toggle lua

---
+++ lua/keymap/terminal.lua
@@ -0,0 +1,26 @@
+local map = vim.keymap.set
+local function split_term()
+	local currentBuff = vim.api.nvim_buf_get_name(0)
+	local term = string.sub(currentBuff, 1, 4)
+	local Isterm = term == "term"
+	print(Isterm)
+	vim.opt.splitright = true
+
+	local cr = vim.api.nvim_replace_termcodes("<CR>", true, true, true)
+	--vim.opt.splitbelow = true
+	if Isterm then
+		vim.api.nvim_feedkeys("a", "t", false)
+		vim.api.nvim_chan_send(vim.bo.channel, "exit")
+		--Simulate pressing the enter key
+		vim.api.nvim_feedkeys(cr, "t", false)
+	end
+	if not Isterm then
+		vim.api.nvim_command('vsplit')
+		vim.api.nvim_command('terminal')
+	end
+end
+
+map("n", "<C-t>", function()
+	split_term()
+end, { noremap = true })
+--------------------------------------------------
